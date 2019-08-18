e48fbc29dcdb03a802d819c91d1e6c7992f8ca00
Nacera HAMMID <cilacila4@gmail.com>
2019-08-18 11:04:12 -0400

    trying to fix cloudinary errors

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -24,6 +24,6 @@ class CocktailsController < ApplicationController
 
   private
   def cocktails_params
-    params.require(:cocktail).permit(:name, :photo)
+    params.require(:cocktail).permit(:name, :photo, :photo_cache)
   end
 end
